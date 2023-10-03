eb3b2f657fcd6d869b8fb9ab431ecdf926451667
lorrehuggan <lorre.huggan@protonmail.com>
2023-10-03 01:09:35 +0100

    update

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "kanagawa",
+  colorscheme = "github_dark_high_contrast",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
